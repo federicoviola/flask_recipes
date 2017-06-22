9f30a13c88d4c96a469863177e7b28f480ea521f
Federico Viola <federicoviola@gmail.com>
2017-06-22 00:22:22 -0300

    Added use of Blueprints to organize project

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,13 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
 
-from . import views
+####################
+#### blueprints ####
+####################
+ 
+from app.users.views import users_blueprint
+from app.recipes.views import recipes_blueprint
+ 
+# register the blueprints
+app.register_blueprint(users_blueprint)
+app.register_blueprint(recipes_blueprint)
