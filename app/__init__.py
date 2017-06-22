1fe85d5827252dbf845a1fea6bb94c5ee484d976
Federico Viola <federicoviola@gmail.com>
2017-06-22 00:51:43 -0300

    Added SQLAlchemy support and file for initializing database

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,16 @@
 #### imports ####
 #################
 from flask import Flask
- 
+from flask_sqlalchemy import SQLAlchemy
+
  ################
  #### config ####
  ################
   
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
+
+db = SQLAlchemy(app)
 
 ####################
 #### blueprints ####
